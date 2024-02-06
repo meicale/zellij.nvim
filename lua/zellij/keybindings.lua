f7d1ef774648cece85a9af6437cf18aef412131f
meicale <bill@DESKTOP-M0LLBM9.>
2024-02-06 11:11:07 +0800

    updates

--- lua/zellij/keybindings.lua
+++ lua/zellij/keybindings.lua
@@ -1,7 +1,9 @@
 local Bindings = {}
 
 function Bindings.setupVimTmuxNavigatorBindings(shouldUseWhichKey)
+    print("setting vim tmux navigator key bindings")
     if shouldUseWhichKey then
+        print("using whichkey")
         local wk = require("which-key")
         wk.register({
             ["<C-h>"] = { "<cmd>ZellijNavigateLeft<cr>", "Navigate left vim window/zellij  pane" },
@@ -10,16 +12,19 @@ function Bindings.setupVimTmuxNavigatorBindings(shouldUseWhichKey)
             ["<C-l>"] = { "<cmd>ZellijNavigateRight<cr>", "Navigate right vim window/zellij pane" },
         })
     else
-        vim.keymap.set('n', '<C-h>', "<cmd>ZellijNavigateLeft<cr>")
-        vim.keymap.set('n', '<C-j>', "<cmd>ZellijNavigateDown<cr>")
-        vim.keymap.set('n', '<C-k>', "<cmd>ZellijNavigateUp<cr>")
-        vim.keymap.set('n', '<C-l>', "<cmd>ZellijNavigateRight<cr>")
+        print("using vim.keymap")
+        vim.keymap.set("n", "<C-h>", "<cmd>ZellijNavigateLeft<cr>")
+        vim.keymap.set("n", "<C-j>", "<cmd>ZellijNavigateDown<cr>")
+        vim.keymap.set("n", "<C-k>", "<cmd>ZellijNavigateUp<cr>")
+        vim.keymap.set("n", "<C-l>", "<cmd>ZellijNavigateRight<cr>")
     end
 end
 
 function Bindings.setupVimWindowBindings(shouldUseWhichKey)
+    print("setting vim window navigator key bindings")
     if shouldUseWhichKey then
         local wk = require("which-key")
+        print("using whichkey")
         wk.register({
             ["<C-w>h"] = { "<cmd>ZellijNavigateLeft<cr>", "Navigate left vim window/zellij  pane" },
             ["<C-w>j"] = { "<cmd>ZellijNavigateDown<cr>", "Navigate down vim window/zellij  pane" },
@@ -27,10 +32,11 @@ function Bindings.setupVimWindowBindings(shouldUseWhichKey)
             ["<C-w>l"] = { "<cmd>ZellijNavigateRight<cr>", "Navigate right vim window/zellij pane" },
         })
     else
-        vim.keymap.set('n', '<C-w>h', "<cmd>ZellijNavigateLeft<cr>")
-        vim.keymap.set('n', '<C-w>j', "<cmd>ZellijNavigateDown<cr>")
-        vim.keymap.set('n', '<C-w>k', "<cmd>ZellijNavigateUp<cr>")
-        vim.keymap.set('n', '<C-w>l', "<cmd>ZellijNavigateRight<cr>")
+        print("using vim.keymap")
+        vim.keymap.set("n", "<C-w>h", "<cmd>ZellijNavigateLeft<cr>")
+        vim.keymap.set("n", "<C-w>j", "<cmd>ZellijNavigateDown<cr>")
+        vim.keymap.set("n", "<C-w>k", "<cmd>ZellijNavigateUp<cr>")
+        vim.keymap.set("n", "<C-w>l", "<cmd>ZellijNavigateRight<cr>")
     end
 end
 
